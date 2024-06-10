f3f18ae4fa06ca03f8cf0dba30a44d51ef97690c
sijin.kim <sijin.kim@yanolja.com>
2024-06-10 13:40:11 +0900

    feat : AcrActions

---
+++ acr/src/main/kotlin/com/koreatlwls/acr/model/AcrActions.kt
@@ -0,0 +1,31 @@
+package com.koreatlwls.acr.model
+
+import kotlinx.collections.immutable.ImmutableList
+
+internal sealed interface AcrActions {
+    sealed interface Navigates : AcrActions {
+        data object Back : Navigates
+    }
+
+    sealed interface Updates : AcrActions {
+        data class RequestQueryValue(val index: Int, val value: String) : Updates
+
+        data class RequestHeaderValue(val index: Int, val value: String) : Updates
+
+        data class RequestBodyValue(
+            val bodyItems: ImmutableList<JsonItem>,
+            val key: String,
+            val newValue: String,
+        )
+
+        data class ResponseBodyValue(
+            val bodyItems: ImmutableList<JsonItem>,
+            val key: String,
+            val newValue: String,
+        )
+
+        data object NewRequest : Updates
+
+        data object NewResponse : Updates
+    }
+}
