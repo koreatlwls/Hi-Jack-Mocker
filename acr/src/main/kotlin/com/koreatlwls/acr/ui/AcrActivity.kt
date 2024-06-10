471c59bec52358a86ba1c9afd6b7d36332338456
sijin.kim <sijin.kim@yanolja.com>
2024-06-10 11:58:58 +0900

    feat : AcrActivity

---
+++ acr/src/main/kotlin/com/koreatlwls/acr/ui/AcrActivity.kt
@@ -0,0 +1,10 @@
+package com.koreatlwls.acr.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class AcrActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
