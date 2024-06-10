fedc705fb780813f5b066440dee11af9f2af9e9f
sijin.kim <sijin.kim@yanolja.com>
2024-06-10 14:12:39 +0900

    feat : RequestScreen, ResponseScreen

--- acr/build.gradle.kts
+++ acr/build.gradle.kts
@@ -36,11 +36,20 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 
     implementation(platform(libs.okhttp.bom))
     implementation(libs.okhttp.core)
---
+++ acr/src/main/kotlin/com/koreatlwls/acr/ui/RequestScreen.kt
@@ -0,0 +1,86 @@
+package com.koreatlwls.acr.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.SemanticsProperties.Text
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.koreatlwls.acr.model.AcrUiState
+import com.koreatlwls.acr.ui.component.KeyValueRow
+
+@Composable
+internal fun RequestScreen(
+    requestUiState: AcrUiState.RequestUiState,
+    onQueryValueChange: (index: Int, value: String) -> Unit,
+    onHeaderValueChange: (index: Int, value: String) -> Unit,
+    onBodyValueChange: (key: String, value: String) -> Unit,
+) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        if (requestUiState.queryKeys.isNotEmpty()) {
+            Text(
+                text = "Query",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+        }
+
+        requestUiState.queryKeys.forEachIndexed { index, key ->
+            KeyValueRow(
+                key = key,
+                value = requestUiState.queryValues[index],
+                onValueChange = {
+                    onQueryValueChange(index, it)
+                }
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+
+        if (requestUiState.headerKeys.isNotEmpty()) {
+            Text(
+                text = "Header",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+        }
+
+        requestUiState.headerKeys.forEachIndexed { index, key ->
+            KeyValueRow(
+                key = key,
+                value = requestUiState.headerValues[index],
+                onValueChange = {
+                    onHeaderValueChange(index, it)
+                }
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+
+        if (requestUiState.bodyItems.isNotEmpty()) {
+            Text(
+                text = "Body",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+        }
+
+        BodyItemList(
+            items = requestUiState.bodyItems,
+            onBodyValueChange = onBodyValueChange,
+        )
+    }
+}
