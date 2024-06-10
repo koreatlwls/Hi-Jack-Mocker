c356bec19202b953c2fd77329f103d5f0e79ccaf
sijin.kim <sijin.kim@yanolja.com>
2024-06-10 13:30:07 +0900

    feat : AcrUiState

---
+++ acr/src/main/kotlin/com/koreatlwls/acr/model/AcrUiState.kt
@@ -0,0 +1,35 @@
+package com.koreatlwls.acr.model
+
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+
+internal data class AcrUiState(
+    val method: String = "",
+    val scheme: String = "",
+    val host: String = "",
+    val path: String = "",
+    val requestUiState: RequestUiState = RequestUiState(),
+    val responseUiState: ResponseUiState = ResponseUiState(),
+){
+    val fullUrl = "$scheme://$host/$path${makeQueries()}"
+
+    private fun makeQueries(): String {
+        return requestUiState.queryKeys.zip(requestUiState.queryValues)
+            .flatMap { (str1, str2) -> listOf("$str1=$str2") }
+            .joinToString("&", prefix = "?")
+    }
+
+    data class RequestUiState(
+        val queryKeys: ImmutableList<String> = persistentListOf(),
+        val queryValues: ImmutableList<String> = persistentListOf(),
+        val headerKeys: ImmutableList<String> = persistentListOf(),
+        val headerValues: ImmutableList<String> = persistentListOf(),
+        val bodyItems: ImmutableList<JsonItem> = persistentListOf(),
+    )
+
+    @JvmInline
+    value class ResponseUiState(
+        val bodyItems: ImmutableList<JsonItem> = persistentListOf(),
+    )
+}
+
