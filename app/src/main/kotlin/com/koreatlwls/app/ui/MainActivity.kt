d6e8e9ace78692793957ae1d337693fd13344a08
sijin.kim <sijin.kim@yanolja.com>
2024-06-12 22:20:04 +0900

    update : mainactivity

--- app/src/main/kotlin/com/koreatlwls/app/ui/MainActivity.kt
+++ app/src/main/kotlin/com/koreatlwls/app/ui/MainActivity.kt
@@ -6,20 +6,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.koreatlwls.acr.ui.AcrActivity
 import com.koreatlwls.app.ui.theme.ApiCustomRequesterTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -38,35 +41,66 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-internal fun MainScreen(viewModel: MainViewModel = hiltViewModel()){
-    val pokemons by viewModel.pokemons.collectAsStateWithLifecycle(
-        lifecycleOwner = androidx.compose.ui.platform.LocalLifecycleOwner.current
-    )
-val context = LocalContext.current
-    Scaffold{
-         LazyColumn(
-             modifier = Modifier
-                 .fillMaxSize()
-                 .padding(it),
-             verticalArrangement = Arrangement.spacedBy(8.dp)
-         ){
-             item{
-                 Button(
-                     onClick = {
-                         val intent = Intent(context, AcrActivity::class.java)
-                         context.startActivity(intent)
-                     }
-                 ){
-                    Text(text = "Start")
-                 }
-             }
+internal fun MainScreen(viewModel: MainViewModel = hiltViewModel()) {
+    val context = LocalContext.current
 
-             items(pokemons.size){
-                 Text(
-                     text = pokemons[it].name,
-                     fontSize = 16.sp,
-                 )
-             }
-         }
+    Scaffold(containerColor = Color.White) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+                .padding(8.dp),
+        ) {
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    val intent = Intent(context, AcrActivity::class.java)
+                    context.startActivity(intent)
+                }
+            ) {
+                Text(text = "Start")
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                LazyColumn(
+                    modifier = Modifier.weight(1f),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    items(viewModel.firstPokemons) { pokemon ->
+                        Text(
+                            text = pokemon.name,
+                            fontSize = 16.sp,
+                        )
+                    }
+                }
+
+                LazyColumn(
+                    modifier = Modifier.weight(1f),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    items(viewModel.secondPokemons) { pokemon ->
+                        Text(
+                            text = pokemon.name,
+                            fontSize = 16.sp,
+                        )
+                    }
+                }
+
+                LazyColumn(
+                    modifier = Modifier.weight(1f),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    items(viewModel.thirdPokemons) { pokemon ->
+                        Text(
+                            text = pokemon.name,
+                            fontSize = 16.sp,
+                        )
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/kotlin/com/koreatlwls/app/ui/MainViewModel.kt
+++ app/src/main/kotlin/com/koreatlwls/app/ui/MainViewModel.kt
@@ -1,12 +1,11 @@
 package com.koreatlwls.app.ui
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.koreatlwls.app.remote.PokemonService
 import com.koreatlwls.app.remote.model.Item
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,18 +14,19 @@ internal class MainViewModel @Inject constructor(
     private val pokemonService: PokemonService,
 ) : ViewModel() {
 
-    private val _pokemons: MutableStateFlow<List<Item>> = MutableStateFlow(emptyList())
-    val pokemons = _pokemons.asStateFlow()
+    val firstPokemons = mutableStateListOf<Item>()
+    val secondPokemons = mutableStateListOf<Item>()
+    val thirdPokemons = mutableStateListOf<Item>()
 
     init {
         viewModelScope.launch {
-            _pokemons.value = pokemonService.getPokemons(0, 20).items
+            firstPokemons.addAll(pokemonService.getPokemons(0, 20).items)
         }
         viewModelScope.launch {
-            _pokemons.value = pokemonService.getPokemons(0, 20).items
+            secondPokemons.addAll(pokemonService.getPokemons(20, 20).items)
         }
         viewModelScope.launch {
-            _pokemons.value = pokemonService.getPokemons(0, 20).items
+            thirdPokemons.addAll(pokemonService.getPokemons(40, 20).items)
         }
     }
 }
