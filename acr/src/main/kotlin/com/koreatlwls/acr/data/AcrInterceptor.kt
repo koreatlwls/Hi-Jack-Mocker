89199bc7981c3555ab9d2409f22029f75a056d71
sijin.kim <sijin.kim@yanolja.com>
2024-06-13 11:24:43 +0900

    feat : startAcrActivityIfNeeded

--- acr/src/main/kotlin/com/koreatlwls/acr/data/AcrInterceptor.kt
+++ acr/src/main/kotlin/com/koreatlwls/acr/data/AcrInterceptor.kt
@@ -1,14 +1,14 @@
 package com.koreatlwls.acr.data
 
+import android.app.ActivityManager
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import com.koreatlwls.acr.ui.AcrActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
 import java.util.concurrent.CompletableFuture
@@ -19,7 +19,7 @@ class AcrInterceptor(
     private val receiveChannel: Channel<Response>,
 ) : Interceptor {
 
-    override fun intercept(chain: Interceptor.Chain): Response  {
+    override fun intercept(chain: Interceptor.Chain): Response {
         val response = chain.proceed(chain.request())
 
         val future = CompletableFuture<Response>()
@@ -32,7 +32,27 @@ class AcrInterceptor(
             future.complete(receiveChannel.receive())
         }
 
+        startAcrActivityIfNeeded(context)
+
         return future.get()
+    }
+
+    private fun startAcrActivityIfNeeded(context: Context) {
+        if (!isAcrActivityRunning(context)) {
+            val intent = Intent(context, AcrActivity::class.java)
+                .apply { addFlags(FLAG_ACTIVITY_NEW_TASK) }
+            context.startActivity(intent)
+        }
+    }
+
+    private fun isAcrActivityRunning(context: Context): Boolean {
+        val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        val runningTasks = activityManager.getRunningTasks(1)
+        if (runningTasks.isNotEmpty()) {
+            val topActivity = runningTasks[0].topActivity
+            return topActivity?.className == AcrActivity::class.java.name
+        }
+        return false
     }
 
 }
