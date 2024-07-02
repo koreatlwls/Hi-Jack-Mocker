a255f48b3a54379bed01fc000909d9820ae51fcb
sijin.kim <sijin.kim@yanolja.com>
2024-07-02 15:16:33 +0900

    Update : icon (#5)

--- hjm/src/main/kotlin/com/koreatlwls/hjm/ui/custom/CustomScreen.kt
+++ hjm/src/main/kotlin/com/koreatlwls/hjm/ui/custom/CustomScreen.kt
@@ -10,7 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -101,7 +101,7 @@ private fun CustomScreen(
                 },
                 navigationIcon = {
                     Icon(
-                        imageVector = Icons.Filled.ArrowBack,
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                         contentDescription = "close",
                         modifier = Modifier.clickable { onActions(CustomActions.Navigates.Back) }
                     )
