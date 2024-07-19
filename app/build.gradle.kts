63ce7ce48726f3bc1ddb238efb515199dd5b2821
sijin.kim <sijin.kim@yanolja.com>
2024-07-19 15:16:24 +0900

    Chore : kotlinComposeCompiler version (#21)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
+        kotlinCompilerExtensionVersion = libs.versions.kotlinComposeCompiler.get()
     }
     packaging {
         resources {
--- hjm/build.gradle.kts
+++ hjm/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
+        kotlinCompilerExtensionVersion = libs.versions.kotlinComposeCompiler.get()
     }
 }
 
